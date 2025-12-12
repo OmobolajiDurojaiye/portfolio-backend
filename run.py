08621f3a61cdd1423517690197aeefb95b76e118
Omobolaji Durojaiye <99079690+OmobolajiDurojaiye@users.noreply.github.com>
2025-12-12 11:17:06 +0100

    configure dotenv for prod

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from app import create_app
+from dotenv import load_dotenv
+import os
+
+# Create the path to the .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+
+# Load the .env file if it exists
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 app = create_app()
 
